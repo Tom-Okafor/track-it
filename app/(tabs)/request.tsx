dd8349a76bb0b7923c6a48460fccbfd59d739319
Peiro Cruz <amapeter21@gmail.com>
2025-12-17 00:19:04 +0100

    chore: reset request page

--- app/(tabs)/request.tsx
+++ app/(tabs)/request.tsx
@@ -1,12 +1,18 @@
+import { Wrapper } from "@/components/layout/wrapper";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 export default function RequestScreen() {
   return (
-    <View>
-      <Text>Request</Text>
-    </View>
+    <Wrapper style={styles.container}>
+      <Text>Test</Text>
+    </Wrapper>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
