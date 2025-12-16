8cb12af32d950d018012470a778f8a4267535d50
Tom Okafor <tmeka008@gmail.com>
2025-12-16 07:11:07 +0100

    feat: configure appwrite client

---
+++ lib/appwrite.ts
@@ -0,0 +1,11 @@
+import { Account, Client, TablesDB } from "react-native-appwrite";
+
+const client = new Client()
+  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT)
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID)
+  .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM);
+
+export const account = new Account(client);
+export const databases = new TablesDB(client);
+
+export default client;
