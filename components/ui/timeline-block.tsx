41e0f9a6486bceff15a2f8df8a190b37a785c76c
Tom Okafor <tmeka008@gmail.com>
2025-12-16 22:40:19 +0100

    feat: create timeline block component

--- components/ui/timeline-block.tsx
+++ components/ui/timeline-block.tsx
@@ -1,8 +1,10 @@
-import { colors } from "@/constants";
-import { scaleFontSize } from "@/utils";
-import { StyleSheet, Text, View } from "react-native";
+import { colors, placeholderTimeLineCardInfo } from "@/constants";
+import { scaleFontSize, scaleVerticalSpacing } from "@/utils";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 import { AuthLink } from "./auth-link";
+import { TimelineBlockCard } from "./timeline-block-card";
 
+const { width } = Dimensions.get("window");
 export function TimelineBlock() {
   return (
     <View style={styles.container}>
@@ -10,14 +12,23 @@ export function TimelineBlock() {
         <Text style={styles.date}>Date</Text>
         <AuthLink text="July, 2015" style={styles.linkStyle} />
       </View>
+      <ScrollView
+        contentContainerStyle={styles.cardBlock}
+        showsVerticalScrollIndicator={false}
+      >
+        {placeholderTimeLineCardInfo.map(({ weekDay, day, current }, index) => (
+          <TimelineBlockCard weekDay={weekDay} day={day} current={current} key={index} />
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    gap: 24,
+    gap: 20,
     width: "100%",
+    flex: 1
   },
   blockHeadText: {
     width: "100%",
@@ -28,10 +39,20 @@ const styles = StyleSheet.create({
   linkStyle: {
     color: colors.black,
     fontFamily: "Poppins-Regular",
+    fontSize: scaleFontSize(12),
   },
   date: {
     color: colors.black,
     fontFamily: "Poppins-Bold",
     fontSize: scaleFontSize(14),
+  },
+  cardBlock: {
+    flexDirection: "row",
+    rowGap: 14,
+    columnGap: 45,
+    flexWrap: "wrap",
+    width: "100%",
+    justifyContent: "center",
+    flexGrow: 1,
   },
 });
